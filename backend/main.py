3334e48308d364057f80806d6eba67f5785ac8c5
Nasrul Huda <nasrul2001@gmail.com>
2025-08-02 13:29:31 +0200

    Template setup

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from backend!")
+import uvicorn
+
+def run_server():
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 if __name__ == "__main__":
-    main()
+    run_server()
