e62a9304c5c80b0fd3fca0c29b8e14dd9dd0f42c
Nasrul Huda <nasrul2001@gmail.com>
2025-08-14 00:04:05 +0530

    Changed config

--- frontend/src/pages/ComingSoonPage.tsx
+++ frontend/src/pages/ComingSoonPage.tsx
@@ -25,12 +25,12 @@ const ComingSoonPage: FC = () => {
         raysColor="#ffffff"
         raysSpeed={1.5}
         lightSpread={0.8}
-        rayLength={1.5}
+        rayLength={2.0}
         followMouse={true}
         mouseInfluence={0.1}
         noiseAmount={0.1}
         distortion={0.05}
-        brightness={4.0}
+        brightness={1.5}
         className="custom-rays"
       />
       <div className="absolute inset-0 flex flex-col items-center justify-center z-10">
